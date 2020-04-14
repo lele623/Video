189d55b3e1731faff349dff37cf9a3c4b9ab0135
admin-get <475912613@qq.com>
2020-04-14 17:06:34 +0800

    完善菜单管理Menu.php

--- application/admin/controller/Menu.php
+++ application/admin/controller/Menu.php
@@ -22,8 +22,43 @@ class Menu extends Baseadmin
         $this->assign('data',$data);
         return $this->fetch();
     }
+    //保存
+    public function save(){
+        $pid =(int)(input('post.pid'));
+        $ords = input('post.ords/a');
+        $titles = input('post.titles/a');
+        $controllers = input('post.controllers/a');
+        $methods = input('post.methods/a');
+        $ishiddens = input('post.hiddens/a');
+        $status = input('post.status/a');
 
-    public function sace(){
+        foreach ($ords as $key => $value){
+            //保存一级列表
+            $data['pid'] = $pid;
+            $data['ord'] = $value;
+            $data['title'] = $titles[$key];
+            $data['controller'] = $controllers[$key];
+            $data['method'] = $methods[$key];
+            $data['ishidden'] = isset($ishiddens[$key]) ? 1 : 0;
+            $data['status'] = isset($status[$key]) ? 1 : 0;
 
+            if($key == 0 && $data['title']){
+                //添加一级目录
+                $this->db->table('admin_menus')->insert($data);
+
+            }
+            if($key > 0){
+                if($data['title'] == '' && $data['controller'] == '' && $data['method'] == ''){
+                    //删除一级目录
+                    $this->db->table('admin_menus')->where(array('mid'=>$key))->delete();
+                }else{
+                    //修改一级目录
+                    $this->db->table('admin_menus')->where(array('mid'=>$key))->update($data);
+
+                }
+            }
+        }
+
+        exit(json_encode(array("code" => 0,"msg" => "保存成功")));
     }
 }
